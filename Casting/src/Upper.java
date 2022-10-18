6e0ad4f4d721146f4c8e130a6212bb991da2abef
Shilpa <knsl.sree@gmail.com>
2022-10-18 22:28:57 +0200

    Created two Projects

---
+++ Casting/src/Upper.java
@@ -0,0 +1,13 @@
+
+public class Upper {
+
+	public static void main(String[] args) {
+		int myInt = 9;
+	    double myDouble = myInt; // Automatic casting: in to double
+
+	    System.out.println(myInt);      // Outputs 9
+	    System.out.println(myDouble);
+
+	}
+
+}
