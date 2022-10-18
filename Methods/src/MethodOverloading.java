1c84d9f88fe226743c5eee5d83689d732e3bb907
Shilpa <knsl.sree@gmail.com>
2022-10-19 00:01:41 +0200

    Recursion is added

---
+++ Methods/src/MethodOverloading.java
@@ -0,0 +1,23 @@
+
+public class MethodOverloading {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		static int plusMethodInt(int x, int y) {
+			  return x + y;
+			}
+
+			static double plusMethodDouble(double x, double y) {
+			  return x + y;
+			}
+
+			public static void main(String[] args) {
+			  int myNum1 = plusMethodInt(8, 5);
+			  double myNum2 = plusMethodDouble(4.3, 6.26);
+			  System.out.println("int: " + myNum1);
+			  System.out.println("double: " + myNum2);
+			}
+	}
+
+}
+}
